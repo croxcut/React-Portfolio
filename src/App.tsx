ec068dde725c0df54eaa9f4e7fc8a7d680ed8dd5
AppaulPie <133219813+croxcut@users.noreply.github.com>
2026-01-04 22:57:04 +0800

    just a practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import TestPage from "./proj/test";
+import TestPage, { USER_PROFILES } from "./components/test";
 
 function App() {
   return (
     <>
-      <TestPage></TestPage>
+      <TestPage user={USER_PROFILES[0 ]}></TestPage>
+      <TestPage user={USER_PROFILES[1]}></TestPage>
     </>
   )
 }
---
+++ src/components/test.tsx
@@ -0,0 +1,34 @@
+import { Profile } from "./profile";
+import styles from "./test.module.css";
+
+export const USER_PROFILES = [
+    new Profile({
+        id: 1,
+        name: 'John Paul',
+        description: 'Person',
+        is_active: true
+    }),
+    new Profile({
+        id: 2,
+        name: 'Evil bitch',
+        description: 'An evil bitch',
+        is_active: false
+    }),
+];
+
+type TestPageProps = {
+  user: Profile;
+};
+
+function TestPage({ user }: TestPageProps) {
+  return (
+    <>
+        <h1 className= {styles.header}>Hello, {user.name}</h1>
+        <pre className= {styles.description}>
+            Description: {user.description}
+        </pre>
+    </>
+  );
+}
+
+export default TestPage;
