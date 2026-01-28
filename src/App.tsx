f67e8c4284e5f60290a8f7b0b7d31d7209a4a1c7
AppaulPie <133219813+croxcut@users.noreply.github.com>
2026-01-28 12:38:45 +0800

    new start

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
 import "./styles/root.css";
-import NavBar from "./modules/Navbar";
-import Introduction from "./modules/Introduction";
-import AboutMe from "./modules/AboutMe";
-import Technologies from "./modules/Technologies";
-import Education from "./modules/Education";
-import GitHubContributions from "./modules/GitHubContributions";
 
 function App() {
   return <>
       <div className="body">
-        <NavBar>
 
-        </NavBar>
-        <Introduction>
-
-        </Introduction>
-        <AboutMe>
-          
-        </AboutMe>
-        <div className="tech_educ">
-          <Education>
-
-          </Education>
-          <Technologies>
-          
-          </Technologies>
-        </div>
-        <GitHubContributions>
-          
-        </GitHubContributions>
       </div>
   </>
 }
